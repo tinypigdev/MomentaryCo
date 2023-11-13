2fbdddba56f83ea44594d63d8906575ed98a77e7
tinypigdev <130195870+tinypigdev@users.noreply.github.com>
2023-11-13 09:42:41 +0800

    Initial Commit

---
+++ MomentaryCo/MomentaryCoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MomentaryCoApp.swift
+//  MomentaryCo
+//
+//  Created by David Higgins on 13/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct MomentaryCoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
